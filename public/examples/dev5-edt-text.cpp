74f4dd42086b288ecc4a137bbf4a6cd8f76bad0c
Fathony L <fathony@smart-leaders.net>
2025-05-11 22:46:06 +0700

    visually realign (by eye)

--- public/examples/dev5-edt-text.cpp
+++ public/examples/dev5-edt-text.cpp
@@ -4,11 +4,7 @@ void setup(void) {
     //u8g2.setFont(u8g2_font_helvB12_tr);
 }
 
-
-// static const unsigned char number_e[] U8X8_PROGMEM = {0, 0, 60, 0, 102, 0, 126, 0, 6, 0, 60, 0, 0, 0};
-// static const unsigned char number_5[] U8X8_PROGMEM = {0x0f,0x01,0x07,0x08,0x09,0x06};
-// static const unsigned char number_123[] U8X8_PROGMEM = {118, 8, 199, 28, 102, 62, 246, 8, 0, 8, 255, 7};
-// static const unsigned char sewing_machine_shoe[] U8X8_PROGMEM = {0xfe,0xff,0xff,0x0f,0x01,0x00,0x00,0x10,0xc1,0x1f,0x03,0x0c,0xc1,0x9f,0x04,0x03,0x01,0x84,0xc4,0x00,0x01,0x84,0x3c,0x00,0x01,0x84,0x00,0x00,0x01,0x84,0x3c,0x00,0x01,0x84,0xc4,0x00,0xc1,0x9f,0x04,0x03,0xc1,0x1f,0x03,0x0c,0x01,0x00,0x00,0x10,0xfe,0xff,0xff,0x0f};
+static const unsigned char img_notebook[] = {3,4,4,4,4,4,3};
 static const unsigned char phonebook_1[] = {0,224,3,0,0,192,1,0,0,32,28,254,0,48,7,0,0,60,224,1,7,200,15,0,0,34,64,0,8,244,27,0,0,66,64,254,9,234,29,0,0,66,64,0,8,197,62,0,0,66,64,254,136,130,47,0,0,66,64,0,76,1,47,0,0,66,64,254,108,1,22,0,0,66,64,0,172,3,20,0,0,194,67,0,180,7,10,0,0,2,92,0,180,13,5,0,0,254,255,255,187,155,2,0,0,252,255,255,121,127,1,0};
 
 void draw(const char *s) {
@@ -16,7 +12,8 @@ void draw(const char *s) {
     u8g2.setDrawColor(1);
 
     u8g2.setFont(nokia_3310_big);
-    u8g2.drawStr(8, 16, "2xClick Me");
+    // u8g2.drawStr(8, 16, "2xClick Me");
+    u8g2.drawStr(6, 18, "Tel. seznam");
 
     u8g2.drawXBM(8, 23, 64, 14, phonebook_1);
 
@@ -24,8 +21,20 @@ void draw(const char *s) {
     u8g2.drawStr(26, 47, "Zvolit");
 
     // u8g2.drawXBM(0, 0, 4, 6, number_5);
-    // u8g2.drawXBM(0, 10, 29, 13, sewing_machine_shoe);
-    u8g2.drawLine(80, 1, 80, 37);
+    u8g2.setFont(nokia_3310_menu);
+    u8g2.drawStr(78, 6, "1");
+    
+    // u8g2.drawLine(81, 8, 82, 8);
+    
+    // u8g2.drawLine(83, 9, 83, 14);
+    
+    // u8g2.drawLine(81, 14, 82, 14);
+
+    u8g2.drawXBM(81, 7, 3, 7, img_notebook);
+
+    u8g2.drawLine(81, 14, 81, 38);
+
+    // u8g2.drawLine(81, 17, 81, 41);
     // u8g2.drawXBM(20, 0, 10, 7, number_e);
     // u8g2.drawXBM(30, 0, 16, 6, number_123);
    
